9ec6bdcf8c0b66958f97724e88f5fb6f481bf64a
dozero25 <ehdud05264@naver.com>
2023-02-22 00:56:30 +0900

    AccountService : 비밀번호 저장할때 encode하고 accountRepository에 있는 saveUser, saveRole에 userMst 저장해서 return 하는 코드 추가
    : UserMst에서 userIdㄹ를 가져와서 findUserByUserId에 accountRepository 넣어서 return 하는 코드 추가
    
    AccountApi
    : accountService에서 userId를 가져오는 코드

--- src/main/java/com/koreait/koreaitsugang/service/AccountService.java
+++ src/main/java/com/koreait/koreaitsugang/service/AccountService.java
@@ -1,8 +1,10 @@
 package com.koreait.koreaitsugang.service;
 
+import com.koreait.koreaitsugang.entity.UserMst;
 import com.koreait.koreaitsugang.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,7 +15,18 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    public UserMst registerUser(UserMst userMst){
+        userMst.setPassword(new BCryptPasswordEncoder().encode(userMst.getPassword()));
 
+        accountRepository.saveUser(userMst);
+        accountRepository.saveRole(userMst);
+
+        return userMst;
+    }
+
+    public UserMst getUser(int userId){
+        return accountRepository.findUserByUserId(userId);
+    }
 
 
 }
--- src/main/java/com/koreait/koreaitsugang/web/api/AccountApi.java
+++ src/main/java/com/koreait/koreaitsugang/web/api/AccountApi.java
@@ -1,15 +1,19 @@
 package com.koreait.koreaitsugang.web.api;
 
+import com.koreait.koreaitsugang.entity.UserMst;
 import com.koreait.koreaitsugang.security.PrincipalDetails;
 import com.koreait.koreaitsugang.service.AccountService;
 import com.koreait.koreaitsugang.web.dto.CMRespDto;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/account")
 public class AccountApi {
+
+    @Autowired
+    private AccountService accountService;
+
+    @ApiResponses({
+            @ApiResponse(code =400, message = "클라이언트가 잘못"),
+            @ApiResponse(code =401, message = "클라이언트가 잘못2")
+    })
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<? extends CMRespDto<? extends UserMst>> getUser(@PathVariable int userId){
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Success", accountService.getUser(userId)));
+    }
 
     @GetMapping("/principal")
     public ResponseEntity<CMRespDto<? extends PrincipalDetails>> getPrincpalDetails(@ApiParam(hidden = true) @AuthenticationPrincipal PrincipalDetails principalDetails) {
