37d35cf47d63203e0806b421c69ee4973599dc73
dozero25 <ehdud05264@naver.com>
2023-03-11 15:37:39 +0900

    11

--- src/main/java/com/koreait/koreaitsugang/config/WebMvcConfig.java
+++ src/main/java/com/koreait/koreaitsugang/config/WebMvcConfig.java
@@ -17,6 +17,7 @@ import java.nio.charset.StandardCharsets;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${file.path}")
     private String filePath;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
